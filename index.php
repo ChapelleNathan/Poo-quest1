27744d322bfc9f31e3272b50fe0d2ec02eed9a83
Chapelle Nathan <nathanchapelle76@gmail.com>
2021-10-10 22:18:01 +0200

    added getters setter

--- index.php
+++ index.php
@@ -144,4 +144,44 @@ class Bicycle
         $sentence .= "Im'stopped !";
         return $sentence;
     }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function setColor($color): void
+    {
+        $this->color = $color;
+    }
+
+    public function getCurrentSpeed(): int
+    {
+        return $this->currentSpeed;
+    }
+
+    public function setCurrentSpeed($currentSpeed): void
+    {
+        $this->currentSpeed = $currentSpeed;
+    }
+
+    public function getNbSeats(): int
+    {
+        return $this->nbSeats;
+    }
+
+    public function setNbSeats($nbSeats): void
+    {
+        $this->nbSeats = $nbSeats;
+    }
+
+    public function getNbWheels(): int
+    {
+        return $this->nbWheels;
+    }
+
+    public function setNbWheels($nbWheels): void
+    {
+        $this->nbWheels = $nbWheels;
+    }
 }
